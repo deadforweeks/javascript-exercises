6f595834ae1849f829ba4c24151203b98b39ff83
DEADFORWEEKS <this.year.ended.in.june@gmail.com>
2023-03-09 06:50:01 -0500

    Update tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function(fa) {
+  var c
+  c = eval( (fa-32)*(5/9) );
+  // console.log(c);
+  return c;
 };
 
-const ctof = function() {
-
+const ctof = function(ce) {
+  var f
+  f = eval ( (ce*9/5)+32 )
+  return f;
 };
 
 // Do not edit below this line
