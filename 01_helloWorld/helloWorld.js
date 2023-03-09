8988132a4e983d1ac37538ea4c0cd7aa66e9a351
DEADFORWEEKS <this.year.ended.in.june@gmail.com>
2023-03-09 07:05:40 -0500

    Update helloWorld.js

--- 01_helloWorld/helloWorld.js
+++ 01_helloWorld/helloWorld.js
@@ -1,5 +1,8 @@
 const helloWorld = function() {
-  return ''
-};
+  return 'Hello, World!'
+}
+console.clear()
+console.log('ready to go.')
+console.log( helloWorld() );
 
 module.exports = helloWorld;
