48a3d87aefa9f75c2235131d0669f7b5ee6ea256
DEADFORWEEKS <this.year.ended.in.june@gmail.com>
2023-03-09 07:03:30 -0500

    Update repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
+function repeatStringNumTimes (string, times) {
+//   create a string that stores all the strings on repeat repeated
+  var repeatedString = "";
+  
+//   while loop , while it is < the times kers
+  while (times > 0) { 
+    repeatedString += string; 
+  //the cache of string stores gets added the string in the argument
+    times--;
+  }
+  
+  //after the while loop:
+  return repeatedString;
+}
 
-};
+thisString = repeatStringNumTimes('dope motherkkers ', 3)
+
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
