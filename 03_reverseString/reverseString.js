8ed07c88353f197977d9a3c91cb3ebc8deeee8fb
DEADFORWEEKS <this.year.ended.in.june@gmail.com>
2023-03-09 07:01:23 -0500

    Update reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
+let myString = "BUNNY is fucking stupid";
 
+const reverseString = function(str) {
+    let splitString = str.split("");
+//     dissect into an as-per-letter array
+  
+  splitString = splitString.reverse()
+//   reversing the array ^
+  
+  splitString = splitString.join("");
+//   Join the array back together
+  
+  return splitString;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
