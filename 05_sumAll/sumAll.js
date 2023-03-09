a9fe02307a9af354037e0c7a3986d97601ba2a96
DEADFORWEEKS <this.year.ended.in.june@gmail.com>
2023-03-09 06:53:30 -0500

    Update sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+//   Sum from a to n = [n * (n + 1) / 2] – [(a - 1) * a / 2]
+function sumAll (a, n) {
+  //a is start number, n is end number
+  var aa =  n*(n+1)/2
+  console.log('first # is ', aa)
+  
+  var bb = (a-1)*a/2
+  console.log('second # is ', bb)
+  
+  var sumResult = aa-bb
+  console.log('return sumResult ',sumResult)
+  
+  return sumResult
+  
+}
 
-};
+console.clear()
+console.log('cleared---')
 
 // Do not edit below this line
 module.exports = sumAll;
